1247b2db31d087409f2b9def9de2560860381323
Arnaud Pineux <arnaud.pineux@acsone.eu>
2021-07-30 10:33:20 +0200

    [IMP] help_online: black, isort, prettier

--- help_online/controllers/help_online_controllers.py
+++ help_online/controllers/help_online_controllers.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -7,9 +6,9 @@ from odoo.http import request
 
 
 class HelpOnlineController(http.Controller):
-
-    @http.route('/help_online/build_url', type='json', auth='user')
+    @http.route("/help_online/build_url", type="json", auth="user")
     def build_url(self, model, view_type, domain=None, context=None):
-        help_online_model = request.env['help.online']
+        help_online_model = request.env["help.online"]
         return help_online_model.get_page_url(
-            model, view_type, domain=domain, context=context)
+            model, view_type, domain=domain, context=context
+        )
--- help_online/wizards/import_help_wizard.py
+++ help_online/wizards/import_help_wizard.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import base64
-from cStringIO import StringIO
-from lxml import etree
 import logging
 import os
+
+from cStringIO import StringIO
+from lxml import etree
 
 from odoo import api, fields, models
 from odoo.tools import convert, misc
@@ -21,9 +21,9 @@ class XmlImport(convert.xml_import):
     """
 
     def _test_xml_id(self, xml_id):
-        if '.' in xml_id:
-            module, _id = xml_id.split('.')
-            if module == '__export__':
+        if "." in xml_id:
+            module, _id = xml_id.split(".")
+            if module == "__export__":
                 return True
         super(XmlImport, self)._test_xml_id(xml_id)
 
@@ -31,7 +31,7 @@ class XmlImport(convert.xml_import):
 class ImportHelpWizard(models.TransientModel):
     _name = "import.help.wizard"
 
-    source_file = fields.Binary('Source File')
+    source_file = fields.Binary("Source File")
 
     @api.multi
     def import_help(self):
@@ -39,15 +39,23 @@ class ImportHelpWizard(models.TransientModel):
             xmlfile = StringIO(base64.decodestring(this.source_file))
             doc = etree.parse(xmlfile)
             relaxng = etree.RelaxNG(
-                etree.parse(
-                    os.path.join(config['root_path'], 'import_xml.rng')))
+                etree.parse(os.path.join(config["root_path"], "import_xml.rng"))
+            )
             try:
                 relaxng.assert_(doc)
             except Exception:
-                _logger.info('The XML file does not fit the required schema !',
-                             exc_info=True)
+                _logger.info(
+                    "The XML file does not fit the required schema !", exc_info=True
+                )
                 _logger.info(misc.ustr(relaxng.error_log.last_error))
                 raise
-            obj = XmlImport(self.env.cr, self._module, idref={}, mode='init',
-                            report=None, noupdate=False, xml_filename=None)
-            obj.parse(doc.getroot(), mode='init')
+            obj = XmlImport(
+                self.env.cr,
+                self._module,
+                idref={},
+                mode="init",
+                report=None,
+                noupdate=False,
+                xml_filename=None,
+            )
+            obj.parse(doc.getroot(), mode="init")
