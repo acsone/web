6cbde46bca02819f0483b242a62c65674ffd1203
Arnaud Pineux <arnaud.pineux@acsone.eu>
2021-08-12 16:35:39 +0200

    [MIG] help_online: Migration to 14.0 - Fix JS with owl

--- help_online/static/src/js/help_online.js
+++ help_online/static/src/js/help_online.js
@@ -1,39 +1,33 @@
 odoo.define("oca.HelpOnline", function (require) {
     "use strict";
 
-    var core = require("web.core");
-    var QWeb = core.qweb;
-    var _t = core._t;
-    var ViewManager = require("web.ViewManager");
-    var ControlPanel = require("web.ControlPanel");
-    var Dialog = require("web.Dialog");
+    const core = require('web.core');
+    const ajax = require('web.ajax');
+    const Dialog = require('web.Dialog');
+    const QWeb = core.qweb;
+    const _t = core._t;
 
-    ControlPanel.include({
-        start: function () {
-            this._super.apply(this, arguments);
-            this._toggle_visibility(true);
-            this.nodes = _.extend(this.nodes, {
-                $help_online_buttons: this.$(".o_help_online_buttons"),
+    const { Component, useState, mount } = owl;
+    const { xml } = owl.tags;
+
+    class HelpOnline extends Component {
+        static template = 'help_online.Button';
+
+        async willStart() {
+            var self = this;
+            return ajax.jsonRpc("/help_online/build_url", 'call', {
+                model: this.env.view.model,
+                view_type: this.env.view.type,
+            }).then(function(result) {
+                self.url_info = result;
             });
-            this._toggle_visibility(false);
-        },
-    });
-
-    ViewManager.include({
-        /**
-         * This function render the help button with the informations received
-         * from the call to the method build_url from the help_online controller
-         */
-        render_help_button: function (url_info) {
-            var $helpButton = $(
-                QWeb.render("HelpOnline.Button", {
-                    view_manager: this,
-                    url_info: url_info,
-                })
-            );
-            $helpButton.tooltip();
-            if (url_info.exists === false) {
-                $helpButton.on("click", function (event) {
+        }
+        mounted() {
+            super.mounted(...arguments);
+            var self = this;
+            var $el = $(self.el);
+            if (self.url_info.exists === false) {
+                $el.on("click", function (event) {
                     var evt = event;
                     evt.preventDefault();
                     Dialog.confirm(
@@ -46,10 +40,17 @@ odoo.define("oca.HelpOnline", function (require) {
                                     id: "formform",
                                     // The location given in the link itself
                                     action: evt.target.href,
-                                    method: "GET",
+                                    method: "POST",
                                     // Open in new window/tab
                                     target: evt.target.target,
                                 });
+                                var csrf = $('<input/>');
+                                csrf.attr({
+                                    type: 'hidden',
+                                    name: 'csrf_token',
+                                    value: core.csrf_token,
+                                })
+                                form.append(csrf);
                                 $("body").append(form);
                                 $("#formform").submit();
                                 $("#formform").remove();
@@ -59,45 +60,16 @@ odoo.define("oca.HelpOnline", function (require) {
                     );
                 });
             }
-            return $helpButton;
-        },
+        }
 
-        /**
-         * This function render the help buttons container on the view.
-         * It should be called after start() by render_view_control_elements.
-         * @param {control_elements} the list of control elements to display into the ControlPanel
-         */
-        render_help_buttons: function (control_elements) {
-            if (!control_elements.$help_online_buttons) {
-                control_elements.$help_online_buttons = $("<div/>");
-            }
-            var self = this;
-            this.rpc("/help_online/build_url", {
-                model: this.dataset.model,
-                view_type: this.active_view.type,
-            }).then(function (result) {
-                if (result && !_.isEmpty(result)) {
-                    var $helpButton = self.render_help_button(result);
-                    control_elements.$help_online_buttons = $helpButton;
-                    // Update the control panel with the new help button
-                    self.update_control_panel(
-                        {
-                            cp_content: _.extend(
-                                {},
-                                self.searchview_elements,
-                                control_elements
-                            ),
-                        },
-                        {clear: false}
-                    );
-                }
-            });
-        },
+    }
 
-        render_view_control_elements: function () {
-            var control_elements = this._super.apply(this, arguments);
-            this.render_help_buttons(control_elements);
-            return control_elements;
-        },
+    const ControlPanel = require("web.ControlPanel");
+
+    ControlPanel.patch("help_online.ControlPanelHelpOnline", (T) => {
+        class ControlPanelPatchRHelpOnline extends T {}
+        ControlPanelPatchRHelpOnline.components.HelpOnline = HelpOnline;
+        return ControlPanelPatchRHelpOnline;
     });
+
 });
